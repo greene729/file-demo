ec56936a0d3ef2620da269dd2e10909765488c0f
Alexander Greene <greene.alexanderj@gmail.com>
2019-04-12 11:48:48 -0400

    INITIAL COMMIT

---
+++ src/main/java/com/greene729/filedemo/FileDemoApplication.java
@@ -0,0 +1,18 @@
+package com.greene729.filedemo;
+
+import com.greene729.filedemo.property.FileStorageProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
+public class FileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/greene729/filedemo/controller/FileController.java
@@ -0,0 +1,14 @@
+package com.greene729.filedemo.controller;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class FileController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+
+}
