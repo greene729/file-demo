7a51990208263221c3a24193c838b9c534226bad
Alexander Greene <greene.alexanderj@gmail.com>
2019-04-25 11:28:20 -0400

    Finish UploadFileResponse and create FileStorageService classes

--- src/main/java/com/greene729/filedemo/payload/UploadFileResponse.java
+++ src/main/java/com/greene729/filedemo/payload/UploadFileResponse.java
@@ -1,4 +1,51 @@
 package com.greene729.filedemo.payload;
 
 public class UploadFileResponse {
+    private String fileName;
+    private String fileDownloadUri;
+    private String fileType;
+    private long size;
+
+    public UploadFileResponse(String fileName, String fileDownloadUri, String fileType, long size) {
+        this.fileName = fileName;
+        this.fileDownloadUri = fileDownloadUri;
+        this.fileType = fileType;
+        this.size = size;
+
+    }
+
+    //Getters and Setters
+
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getFileDownloadUri() {
+        return fileDownloadUri;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setFileDownloadUri(String fileDownloadUri) {
+        this.fileDownloadUri = fileDownloadUri;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
 }
